5d9133912a36beda016952b1f7592490342f5f31
malak-majdoub <malak.majdoub@motopp.nl>
2026-05-06 17:46:44 +0200

    ci: grant access

---
+++ infra/sql/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 \
+  --username "$POSTGRES_USER" \
+  --dbname "$POSTGRES_DB" <<-EOSQL
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL ON TABLES TO ${POSTGRES_USER};
+
+EOSQL
